871ef23b488b33b448bab0d95b7ee254164185d5
Bryan <bryansugi@gmail.com>
2023-04-01 18:44:20 -0500

    Add: se logra exportar el modulo de pokemons

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokemonResponse } from './interfaces/poke-response-interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
     const { data } = await this.axios.get<PokemonResponse>(
@@ -14,7 +22,6 @@ export class SeedService {
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = segments[segments.length - 2];
-      console.log({ name, no });
     });
 
     return data.results;
