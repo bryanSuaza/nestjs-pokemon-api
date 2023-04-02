52a1a39cf833865fca388e258d3822efa56c3cdc
Bryan <bryansugi@gmail.com>
2023-04-01 20:16:21 -0500

    Add: se hace api para inserción de semillas o seeds

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokemonResponse } from './interfaces/poke-response-interface';
@@ -15,15 +16,43 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
+
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokemonResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    // const insertPromisesArray = [];
+    const pokemonToInsert: {name: string, no: number}[] = [];
+
+    data.results.forEach(async({ name, url }) => {
       const segments = url.split('/');
-      const no = segments[segments.length - 2];
+      const no = +segments[segments.length - 2];
+      
+      // inserta uno a uno
+      //await this.pokemonModel.create({name, no});
+
+      // inserta la promesa en un array
+      /* insertPromisesArray.push(
+        this.pokemonModel.create({name, no})
+      ); */
+
+      // forma optima: vamnos insertrando datos al array
+      pokemonToInsert.push({ name, no });
     });
 
-    return data.results;
+    // ejecuta el array de promesas
+    // await Promise.all(insertPromisesArray);
+    // insertamos el dato uno a uno
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+
+    const pokemons = await this.pokemonModel.find();
+
+    return {
+      response: 'Seed execute',
+      pokemons
+    };
   }
 }
